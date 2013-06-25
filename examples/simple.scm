cc2ee1128583b44357827ab2f0c7a692463359c9
David Thompson <dthompson2@worcester.edu>
2013-06-25 07:44:02 -0400

    Update simple demo.

--- examples/simple.scm
+++ examples/simple.scm
@@ -2,20 +2,22 @@
              (figl gl)
              (2d sprite)
              (2d game-loop)
-             (2d window)
-             (2d vector))
+             (2d window))
 
 (define window-width 800)
 (define window-height 600)
 (define sprite #f)
 
 (define (key-down key)
+  (display key) (newline)
   (case key
     ;; Quit program when ESCAPE or Q is pressed.
+    ;; For now we have to use the SDL keycodes, but not for long!
     ((SDLK_ESCAPE SDLK_q)
      (close-window)
      (quit))))
 
+;; Draw our sprite
 (define (render)
   (draw-sprite sprite))
 
@@ -26,7 +28,7 @@
 ;; Open the window.
 (open-window window-width window-height)
 
-;; Load a sprite.
+;; Load a sprite and center it on the screen.
 ;; Must be done AFTER opening the window.
 (set! sprite (load-sprite "sprite.png" #:position (vector (/ window-width 2)
                                                           (/ window-height 2))))
